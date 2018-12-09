8781d59558b5a99d087dc306cc42b86163839f2c
Timothy VanZant <usul@Usuls-MacBook-Air.local>
2018-12-09 13:28:10 -0600

    updated main

--- nine/nine.cc
+++ nine/nine.cc
@@ -1,26 +1,15 @@
-#include "Population.h"
-
-// float minfun(std::vector<float> x)  {
-// 	return std::pow(x[0]-0.11,2.0)
-// 	       +std::pow(x[1]-7.0,2.0)
-//                +std::pow(x[2]-89.4,2.0);
-// }
+#include <iostream>
+#include "GeneticAlgorithm.h"
+#include "Objective.h"
 
 int main() {
-  int popnum = 5;
-  int gencount = 1;
-  int best;
-
-  Population pop = Population(popnum);
-
-  for (int g=0; g < gencount; ++g) {
-    std::vector<float> O(popnum);
-    for (int i=0; i<popnum; ++i) O[i]=minfun(pop.pop[i]);
-
-    std::cout << "\nGeneration " << g << "\n";
-    for (auto o : O) std::cout << o << " ";
-		std::cout << "\n";
-
-    pop.selectParents(O);
+  GeneticAlgorithm ga(2,4,16);
+  
+  std::vector<float> O;
+  
+  O = ga.process(Min::objective);
+  for (int i = 0; i < O.size(); i++) {
+    std::cout << O[i] << "\n";
   }
+
 }
